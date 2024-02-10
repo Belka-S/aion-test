3e9178b8cf1080c62e5dc825d6ef12d60a7def97
Belka <belka.srh.it@gmail.com>
2024-02-10 12:25:31 +0100

    update: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,33 +1,33 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path');
+// const path = require('path');
 
 const nextConfig = {
   output: 'export',
 
-  images: {
-    unoptimized: false,
-    // domains: [ 'images.unsplash.com', 'res.cloudinary.com', 'img.etimg.com', 'assets.vogue.com', 'm.media-amazon.com', 'upload.wikimedia.org', ],
+  // images: {
+  //   unoptimized: false,
+  //   domains: [ 'images.unsplash.com', 'res.cloudinary.com', 'img.etimg.com', 'assets.vogue.com', 'm.media-amazon.com', 'upload.wikimedia.org', ],
 
-    deviceSizes: [320, 480, 768, 1080, 1200, 1440, 1920, 2048, 3840],
+  //   deviceSizes: [320, 480, 768, 1080, 1200, 1440, 1920, 2048, 3840],
 
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-    ],
-  },
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: '**',
+  //     },
+  //     {
+  //       protocol: 'http',
+  //       hostname: 'localhost',
+  //     },
+  //   ],
+  // },
 
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
-  },
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'src/styles')],
+  // },
 
-  reactStrictMode: false,
+  // reactStrictMode: false,
 };
 
 module.exports = nextConfig;
